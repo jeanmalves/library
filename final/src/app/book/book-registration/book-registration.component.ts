c295f6399a5b4725d409295ee29adff07c304abf
Iago Barboza <iagobarboza@gmail.com>
2018-10-08 12:19:06 -0300

    Redirects to /books if currentUser is available and pathname is /

---
+++ final/src/app/book/book-registration/book-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ead-book-registration',
+  templateUrl: './book-registration.component.html',
+  styleUrls: ['./book-registration.component.css']
+})
+export class BookRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- final/src/app/book/book.module.ts
+++ final/src/app/book/book.module.ts
@@ -4,13 +4,14 @@ import { BooksComponent } from './books/books.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BooksListComponent } from './books-list/books-list.component';
 import { BookService } from './book.service';
+import { BookRegistrationComponent } from './book-registration/book-registration.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule
   ],
-  declarations: [BooksComponent, BooksListComponent],
+  declarations: [BooksComponent, BooksListComponent, BookRegistrationComponent],
   providers: [BookService]
 })
 export class BookModule { }
